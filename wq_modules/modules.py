1f5e8ea5e8e71173c5cd1936d0db079157fab7f3
ferag <aguilarf@ifca.unican.es>
2020-02-26 10:07:16 +0000

    Deleted unused packages

--- wq_modules/modules.py
+++ wq_modules/modules.py
@@ -14,29 +14,11 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from wq_modules import sentinel as s2
-from wq_modules import clouds
-from wq_modules import water
 from wq_modules import config
 from wq_modules import metadata_gen
 from wq_modules import meteo
 from wq_modules import repos
 from wq_modules import config
-
-def get_sentinel2_raw(inidate,enddate,region):
-  return s2._init_(inidate,enddate,region)
-
-def cloud_coverage(image):
-  return clouds.cloud_coverage(image)
-
-def cloud_mask(image):
-  return clouds.cloud_mask(image)
-
-def water_surface(image):
-  return water.water_surface(image)
-
-def water_mask(image):
-  return water.water_mask(image)
 
 def metadata_gen(title,dateIni,dateEnd,geographicDesc,westBounding,eastBounding,northBounding,southBounding,params):
   return metadata_gen.metadata_gen(title,dateIni,dateEnd,geographicDesc,westBounding,eastBounding,northBounding,southBounding,params)
